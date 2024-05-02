699d15598ab0d3cb5fb4f7b13778dc8181c5d640
heosuyeon <hsy121891@gmail.com>
2024-05-02 20:13:25 +0900

    PWA 설정 파일 추가

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import "./assets/css/normalize.css";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 // 배포시 console 지우기
 if (process.env.NODE_ENV === "production") {
@@ -18,3 +19,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
